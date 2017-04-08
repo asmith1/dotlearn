315619fa795e4bea1071992e00bf332b724b5a62
Ashley Smith <Ashley@Ashleys-MacBook-Pro-6.local>
2017-04-08 11:11:19 -0400

    fixed cors?

--- backend/index.js
+++ backend/index.js
@@ -4,11 +4,11 @@ var app = express();
 app.set('port', (process.env.PORT || 5000));
 
 /* DO WE NEED CORS? let's assume yes! */
-app.all('/', function(request, response, next) {
+app.use(function(request, response, next) {
   response.header("Access-Control-Allow-Origin", "*");
   response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  res.header("Access-Control-Allow-Headers", "GET, PUT, POST, DELETE");
+  response.header("Access-Control-Allow-Headers", "X-Requested-With");
+  response.header("Access-Control-Allow-Headers", "GET, PUT, POST, DELETE");
   next();
 }); 
 
@@ -18,7 +18,6 @@ var MongoClient = require('mongodb').MongoClient, format = require('util').forma
 var db = MongoClient.connect(mongoUri, function(error, databaseConnection) {
     db = databaseConnection;
 });
-
 
 app.post('/post.json', function(request, response) {
  // response.send("POST TEST");
