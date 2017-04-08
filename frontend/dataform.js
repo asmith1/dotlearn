0f01bfc8f0802ee939185a915231e35959debfdc
Ashley Smith <Ashley@Ashleys-MacBook-Pro-6.local>
2017-04-08 03:33:04 -0400

    edited dataform.js

--- frontend/dataform.js
+++ frontend/dataform.js
@@ -6,6 +6,6 @@ script.type = 'text/javascript';
 function sendData(score, grid)
 {
     //newUsername = window.prompt("Please enter your username", "username");
-    $.post( "https://sleepy-depths-86014.herokuapp.com/submit.json", {"username":newUsername, "score": score, "grid":JSON.stringify(grid)},function(result) { 
+    $.post( "https://sleepy-depths-86014.herokuapp.com/submit.json", {"lat":lat, "lng": lng},function(result) { 
     });
 }
