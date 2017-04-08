d6d9dc84a7ef4e694c8c073cbe535c2fdbac2950
Ashley Smith <Ashley@Ashleys-MacBook-Pro-6.local>
2017-04-08 02:43:29 -0400

    updated dataform.html

--- frontend/dataform.js
+++ frontend/dataform.js
@@ -1,6 +1,11 @@
+var script = document.createElement('script');
+script.src = 'http://code.jquery.com/jquery-1.11.0.min.js';
+script.type = 'text/javascript';
+
+
 function sendData(score, grid)
 {
-    newUsername = window.prompt("Please enter your username", "username");
+    //newUsername = window.prompt("Please enter your username", "username");
     $.post( "https://sleepy-depths-86014.herokuapp.com/submit.json", {"username":newUsername, "score": score, "grid":JSON.stringify(grid)},function(result) { 
     });
 }
