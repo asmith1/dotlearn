dfebd396e19f3dd759f6c50e96dde1460aaf1bda
Ashley Smith <Ashley@Ashleys-MacBook-Pro-6.local>
2017-04-08 00:19:10 -0400

    updated js

--- mapscript.js
+++ mapscript.js
@@ -4,4 +4,8 @@ function initmap()
     ghanalat = 7.892899;
     ghanalon = -1.048289;
     ghanacntr = new google.maps.LatLng(ghanalat, ghanalon);
+    map = new google.maps.Map(document.getElementById('ghanacntr'), {
+          zoom: 8,
+          center: ghanacntr
+    });
 }
