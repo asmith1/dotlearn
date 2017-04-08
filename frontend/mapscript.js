cfe2c9422fc3813d5a08c59f1f4df7aeecf7d958
Uku-Kaspar Uustalu <ukukaspar.uustalu@gmail.com>
2017-04-08 01:33:17 -0400

    fixed map

--- frontend/mapscript.js
+++ frontend/mapscript.js
@@ -1,11 +1,19 @@
-function initmap()
+var ghanaLat = 7.892899;
+var ghanaLng = -1.048289;
+
+var ghanaCtr;
+var map;
+
+var mapOptions = {
+	zoom: 5,
+	center: ghanaCtr,
+	mapTypeId: google.maps.MapTypeId.ROADMAP
+}
+
+
+function init()
 {
-    infowindow = new google.maps.InfoWindow();
-    ghanalat = 7.892899;
-    ghanalon = -1.048289;
-    ghanacntr = new google.maps.LatLng(ghanalat, ghanalon);
-    map = new google.maps.Map(document.getElementById('ghanacntr'), {
-          zoom: 8,
-          center: ghanacntr
-    });
+    map = new google.maps.Map(document.getElementById("map"), mapOptions);
+    ghanaCtr = new google.maps.LatLng(ghanaLat, ghanaLng);
+    map.panTo(ghanaCtr);
 }
